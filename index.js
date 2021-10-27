45a3c97e245c076b5490cd845d7b8232fdabfc49
Logan Young <logany@att.net>
2021-10-27 18:58:16 -0500

    core lab completed

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log("hello world");
+ console.log("hello world");
+let message = document.querySelector('#message')
+let addMovie = (event) => {
+    event.preventDefault();
+    let inputField = document.querySelector("input");
+    let movie = document.createElement("li");
+    let movieTitle = document.createElement("span");
+    movieTitle.textContent = inputField.value;
+    movieTitle.addEventListener('click', crossOffMovie)
+    
+    movie.appendChild(movieTitle);
+    let deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "x";
+    deleteBtn.addEventListener('click', deleteMovie);
+    movie.appendChild(deleteBtn);
+    
+    let list = document.querySelector('ul')
+    list.appendChild(movie);
+    inputField.value = "";
+    }
+    
+document.querySelector('form').addEventListener('submit', addMovie);
+
+let deleteMovie = event => {
+    event.target.parentNode.remove();
+    message.textContent = `that one has been deleted!`
+}
+
+let crossOffMovie = event => {
+    event.target.classList.toggle("checked");
+    if(event.target.classList.contains('checked')){
+        message.textContent = 'this movie was watched!'
+    } else {
+        message.textContent = 'this movie was added back!'
+    }
+}
